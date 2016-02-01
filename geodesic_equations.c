cfcc1a454c639a70617514d2a20b91a97c418740
CesarArroyo09 <cesararroyocardenas@gmail.com>
2016-02-01 16:30:27 -0500

    Interpolation and functions debugged Functions were debugged and the interpolation in this branch is made by every time calling gsl objects (NOT WHAT I WANT). About to merge with test branch.

--- geodesic_equations.c
+++ geodesic_equations.c
@@ -75,7 +75,7 @@ double geodesic_equation_0(double aprime, double a, double p0, double p1, double
 /*Function for the ith differential equation for the geodesics, where i=1,2,3.
 xi is the ith coordinate, pi is the ith momentum.
 The other position and momentum quantities are denoted xj1, xj2, pj1 and pj2. These quantities appear in a symmetric way in the equation so in doesn't matter the order.*/
-double geodesic_equation_i(double aprime, double a, double p0, double pi, double pj1, double pj2, double pj3, double eta, double xi, double xj1, double xj2)
+double geodesic_equation_i(double aprime, double a, double p0, double pi, double pj1, double pj2, double eta, double xi, double xj1, double xj2)
 {
   double f =  -ith_der_potential(xi,xj1,xj2)*pow(p0,2) - 2*(aprime/a)*p0*pi + ith_der_potential(xi,xj1,xj2)*(pow(pi,2)-pow(pj1,2)-pow(pj2,2)) + 2*ith_der_potential(xj1,xi,xj2)*pi*pj1 + 2*ith_der_potential(xj2,xi,xj1)*pi*pj2;
   return f;
@@ -88,10 +88,10 @@ void euler1(double a, double aprime, double *eta, double *x1, double *x2, double
 
   /*Calculation of the increments*/
   deta = *p0*DLAMBDA; dx1 = *p1*DLAMBDA; dx2 = *p2*DLAMBDA; dx3 = *p3*DLAMBDA;
-  dp0 = geodesic_equation_0(aprime, a, *p0, *p1, *p2, *p3, *eta, *x1, *x2, *x3, *x4)*DLAMBDA;
-  dp1 = geodesic_equation_i(aprime, a, *p0, *p1, *p2 *p3, *eta, *x1, *x2, *x3)*DLAMBDA;
-  dp2 = geodesic_equation_i(aprime, a, *p0, *p2, *p1 *p3, *eta, *x2, *x1, *x3)*DLAMBDA;
-  dp3 = geodesic_equation_i(aprime, a, *p0, *p3, *p2 *p1, *eta, *x3, *x2, *x1)*DLAMBDA;
+  dp0 = geodesic_equation_0(aprime, a, *p0, *p1, *p2, *p3, *eta, *x1, *x2, *x3)*DLAMBDA;
+  dp1 = geodesic_equation_i(aprime, a, *p0, *p1, *p2, *p3, *eta, *x1, *x2, *x3)*DLAMBDA;
+  dp2 = geodesic_equation_i(aprime, a, *p0, *p2, *p1, *p3, *eta, *x2, *x1, *x3)*DLAMBDA;
+  dp3 = geodesic_equation_i(aprime, a, *p0, *p3, *p2, *p1, *eta, *x3, *x2, *x1)*DLAMBDA;
 
   /*New values of the variables of the differential equation. Since we are using pointers, when called the routine the value of variable change.*/
   *eta = *eta + deta; *x1 = *x1 + dx1; *x2 = *x2 + dx2; *x3 = *x3 + dx3;
@@ -119,24 +119,26 @@ int main(void)
     }
   
   /*Initial conditions*/
-  double eta = 0, x1 = 0, x2 = 0, x3 = 0, p0 = 0, p1 = 0, p2 = 0, p3 = 0, lambda = 0;
+  double eta = 0.2; // x1 = 0, x2 = 0, x3 = 0, p0 = 0, p1 = 0, p2 = 0, p3 = 0, lambda = 0;
 
   /*Initial values for scale factor and derivative of scale factor*/
   double a = interp_scale_factor(conftime, scale_factor, eta);
   double aprime = interp_der_scale_factor(conftime, der_scale_factor, eta);
 
+  printf("%.12lf %.12lf\n",a,aprime);
+
   /*Pointer to file where solution of differential equation will be saved.*/
-  FILE *geodesic;
-  geodesic = fopen("geodesic_solution.dat","w");
+  //FILE *geodesic;
+  //geodesic = fopen("geodesic_solution.dat","w");
 
   /*Write line of initial values in file*/
-  fprintf(geodesic, "%12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf", lambda, eta, x1, x2, x3, p0, p1, p2, p3);
+  //fprintf(geodesic, "%12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf", lambda, eta, x1, x2, x3, p0, p1, p2, p3);
 
   /*Solution of the differential equation*/
-  for(i=0; i<1000; i++)
-    {
-      euler1(a, aprime, &eta, &x1, &x2, &x3, &p0, &p1, &p2, &p3, &lambda);
-      a = interp_scale_factor(conftime, scale_factor, eta);
-      aprime = interp_der_scale_factor(conftime, der_scale_factor, eta);
-    }
+  //for(i=0; i<1000; i++)
+  //  {
+  //    euler1(a, aprime, &eta, &x1, &x2, &x3, &p0, &p1, &p2, &p3, &lambda);
+  //    a = interp_scale_factor(conftime, scale_factor, eta);
+  //    aprime = interp_der_scale_factor(conftime, der_scale_factor, eta);
+  //  }
 }
