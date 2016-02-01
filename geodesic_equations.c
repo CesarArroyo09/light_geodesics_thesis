e11d23415006797c2387f29851a7c3e39e0297e7
CesarArroyo09 <cesararroyocardenas@gmail.com>
2016-02-01 15:06:13 -0500

    Attempt to change interpolation functions

--- geodesic_equations.c
+++ geodesic_equations.c
@@ -16,36 +16,18 @@ Where $p^{\alpha}={\dot{x}}^{\alpha}$*/
 
 /*Interpolation of scale factor at time eta.
 Argument conf_time is an array with the conformal times. scale_factor is an array of scale factors which corresponds in array position to the conformal time array.*/
-double interp_scale_factor(double conf_time[], double scale_factor[], double eta)
+double interp_scale_factor(gsl_spline *spline, double eta, gsl_interp_accel *acc)
 {
-  /*Allocate space in memory*/
-  gsl_interp_accel *acc = gsl_interp_accel_alloc();  //Acceleration type object (for index lookup)
-  gsl_spline *spline = gsl_spline_alloc(gsl_interp_cspline, NLINES);  //Spline type object (define interpolation type and space in memory)
-
-  gsl_spline_init(spline, conf_time, scale_factor, NLINES);  //Initializes spline object for data conf_time, scale_factor of size NLINES
   double a = gsl_spline_eval(spline, eta, acc);  //Interpolates data to abcisa eta using method in spline and acceleration object acc
-  
-  /*Free space in memory*/
-  gsl_spline_free(spline);  //Free memory of spline object
-  gsl_interp_accel_free(acc);  //Free memory of accel object
 
   return a;  //Return scale factor at a time eta
 }
 
 /*Interpolation of derivative of scale factor at time eta.
 This function works the same way as interp_scale_factor.*/
-double interp_der_scale_factor(double conf_time[], double der_scale_factor[], double eta)
+double interp_der_scale_factor(gsl_spline *spline, double eta, gsl_interp_accel *acc)
 {
-  /*Allocate space in memory*/
-  gsl_interp_accel *acc = gsl_interp_accel_alloc();  //Acceleration type object (for index lookup)
-  gsl_spline *spline = gsl_spline_alloc(gsl_interp_cspline, NLINES);  //Spline type object (define interpolation type and space in memory)
-
-  gsl_spline_init(spline, conf_time, der_scale_factor, NLINES);  //Initializes spline object for data conf_time, der_scale_factor of size NLINES
   double adot = gsl_spline_eval(spline, eta, acc);  //Interpolates data to abcisa eta using method in spline and acceleration object acc
-  
-  /*Free space in memory*/
-  gsl_spline_free(spline);  //Free memory of spline object
-  gsl_interp_accel_free(acc);  //Free memory of accel object
 
   return adot;  //Return derivative of scale factor at a time eta
 }
@@ -75,7 +57,7 @@ double geodesic_equation_0(double aprime, double a, double p0, double p1, double
 /*Function for the ith differential equation for the geodesics, where i=1,2,3.
 xi is the ith coordinate, pi is the ith momentum.
 The other position and momentum quantities are denoted xj1, xj2, pj1 and pj2. These quantities appear in a symmetric way in the equation so in doesn't matter the order.*/
-double geodesic_equation_i(double aprime, double a, double p0, double pi, double pj1, double pj2, double pj3, double eta, double xi, double xj1, double xj2)
+double geodesic_equation_i(double aprime, double a, double p0, double pi, double pj1, double pj2, double eta, double xi, double xj1, double xj2)
 {
   double f =  -ith_der_potential(xi,xj1,xj2)*pow(p0,2) - 2*(aprime/a)*p0*pi + ith_der_potential(xi,xj1,xj2)*(pow(pi,2)-pow(pj1,2)-pow(pj2,2)) + 2*ith_der_potential(xj1,xi,xj2)*pi*pj1 + 2*ith_der_potential(xj2,xi,xj1)*pi*pj2;
   return f;
@@ -88,10 +70,10 @@ void euler1(double a, double aprime, double *eta, double *x1, double *x2, double
 
   /*Calculation of the increments*/
   deta = *p0*DLAMBDA; dx1 = *p1*DLAMBDA; dx2 = *p2*DLAMBDA; dx3 = *p3*DLAMBDA;
-  dp0 = geodesic_equation_0(aprime, a, *p0, *p1, *p2, *p3, *eta, *x1, *x2, *x3, *x4)*DLAMBDA;
-  dp1 = geodesic_equation_i(aprime, a, *p0, *p1, *p2 *p3, *eta, *x1, *x2, *x3)*DLAMBDA;
-  dp2 = geodesic_equation_i(aprime, a, *p0, *p2, *p1 *p3, *eta, *x2, *x1, *x3)*DLAMBDA;
-  dp3 = geodesic_equation_i(aprime, a, *p0, *p3, *p2 *p1, *eta, *x3, *x2, *x1)*DLAMBDA;
+  dp0 = geodesic_equation_0(aprime, a, *p0, *p1, *p2, *p3, *eta, *x1, *x2, *x3)*DLAMBDA;
+  dp1 = geodesic_equation_i(aprime, a, *p0, *p1, *p2, *p3, *eta, *x1, *x2, *x3)*DLAMBDA;
+  dp2 = geodesic_equation_i(aprime, a, *p0, *p2, *p1, *p3, *eta, *x2, *x1, *x3)*DLAMBDA;
+  dp3 = geodesic_equation_i(aprime, a, *p0, *p3, *p2, *p1, *eta, *x3, *x2, *x1)*DLAMBDA;
 
   /*New values of the variables of the differential equation. Since we are using pointers, when called the routine the value of variable change.*/
   *eta = *eta + deta; *x1 = *x1 + dx1; *x2 = *x2 + dx2; *x3 = *x3 + dx3;
@@ -118,25 +100,48 @@ int main(void)
       fscanf(frw,"%lf %lf %lf %lf", &cosmictime, &conftime[i], &scale_factor[i], &der_scale_factor[i]);
     }
   
+  /*** Initializes objects for interpolation. 1 is for interpolation of scale factor, 2 is for interpolation of derivative of scale factor ***/
+
+  /*Allocate space in memory*/
+  gsl_interp_accel *acc1 = gsl_interp_accel_alloc();  //Acceleration type object (for index lookup)
+  gsl_interp_accel *acc2 = gsl_interp_accel_alloc();
+  gsl_spline *spline1 = gsl_spline_alloc(gsl_interp_cspline, NLINES);  //Spline type object (define interpolation type and space in memory, works for both)
+  gsl_spline *spline2 = gsl_spline_alloc(gsl_interp_cspline, NLINES);
+
+  /*Initializes objects for interpolation*/
+  gsl_spline_init(spline1, conftime, scale_factor, NLINES);  //Initializes spline object for data conf_time, scale_factor of size NLINES
+  gsl_spline_init(spline2, conftime, der_scale_factor, NLINES);  //Initializes spline object for data conf_time, der_scale_factor of size NLINES
+
+  /************************************************************************************/
+
   /*Initial conditions*/
-  double eta = 0, x1 = 0, x2 = 0, x3 = 0, p0 = 0, p1 = 0, p2 = 0, p3 = 0, lambda = 0;
+  double eta = 0.1; // x1 = 0, x2 = 0, x3 = 0, p0 = 0, p1 = 0, p2 = 0, p3 = 0, lambda = 0;
 
   /*Initial values for scale factor and derivative of scale factor*/
-  double a = interp_scale_factor(conftime, scale_factor, eta);
-  double aprime = interp_der_scale_factor(conftime, der_scale_factor, eta);
+  double a = interp_scale_factor(spline1, eta, acc1);
+  double aprime = interp_der_scale_factor(spline2, eta, acc2);
+
+  printf("%.12lf %.12lf",a,aprime);
 
   /*Pointer to file where solution of differential equation will be saved.*/
   FILE *geodesic;
   geodesic = fopen("geodesic_solution.dat","w");
 
   /*Write line of initial values in file*/
-  fprintf(geodesic, "%12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf", lambda, eta, x1, x2, x3, p0, p1, p2, p3);
+  //fprintf(geodesic, "%12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf %12lf", lambda, eta, x1, x2, x3, p0, p1, p2, p3);
 
   /*Solution of the differential equation*/
-  for(i=0; i<1000; i++)
-    {
-      euler1(a, aprime, &eta, &x1, &x2, &x3, &p0, &p1, &p2, &p3, &lambda);
-      a = interp_scale_factor(conftime, scale_factor, eta);
-      aprime = interp_der_scale_factor(conftime, der_scale_factor, eta);
-    }
+  //for(i=0; i<1000; i++)
+  //  {
+  //    euler1(a, aprime, &eta, &x1, &x2, &x3, &p0, &p1, &p2, &p3, &lambda);
+  //    a = interp_scale_factor(conftime, scale_factor, eta);
+  //    aprime = interp_der_scale_factor(conftime, der_scale_factor, eta);
+  //  }
+
+  /** Releasing all used space in memory **/
+  fclose(geodesic); //Close file storing the results
+  gsl_spline_free(spline1);  //Free memory of spline object
+  gsl_spline_free(spline2);
+  gsl_interp_accel_free(acc1);  //Free memory of accel object
+  gsl_interp_accel_free(acc2);
 }
