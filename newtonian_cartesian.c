b5ce6ee9b039fa213f5f948d8fa7136fa1250821
CesarArroyo09 <cesararroyocardenas@gmail.com>
2016-03-21 10:04:01 -0500

    Set of equations for perturbed Minkowski geodesics changed Set of equations have been changed, where the program solves for the variables (x0,x1,x2,x3) = (ct,x,y,z)

--- newtonian_cartesian.c
+++ newtonian_cartesian.c
@@ -9,7 +9,7 @@ Where $p^{\alpha}={\dot{x}}^{\alpha}$*/
 
 #define A 1.0     //Distance parameter of the perturbations
 #define G 43007.01     //Gravitational constant
-#define M 5000.0     //Mass of the perturbation
+#define M 0.0     //Mass of the perturbation
 #define C 300000.0  //Speed of light
 #define NLINES 999 //Number of lines in frw.dat file
 #define DLAMBDA 0.000001   //Geodesics parameter step
@@ -32,36 +32,35 @@ mydbl ith_der_potential(mydbl xi, mydbl xj1, mydbl xj2)
 
 /*This is the function of the first differential equation for the geodesics.
 l^{dot} = f1(variables of the problem)*/
-mydbl geodesic_equation_0(mydbl p0, mydbl p1, mydbl p2, mydbl p3, mydbl t, mydbl x1, mydbl x2, mydbl x3)
+mydbl geodesic_equation_0(mydbl p0, mydbl p1, mydbl p2, mydbl p3, mydbl x0, mydbl x1, mydbl x2, mydbl x3)
 {
-  mydbl f = - (2/powl(C,2))*p0*(p1*ith_der_potential(x1, x2, x3) + p2*ith_der_potential(x2, x1, x3) + p3*ith_der_potential(x3, x2, x1));
+  mydbl f = - p0*(p1*ith_der_potential(x1, x2, x3) + p2*ith_der_potential(x2, x1, x3) + p3*ith_der_potential(x3, x2, x1));
   return f;
 }
 
 /*Function for the ith differential equation for the geodesics, where i=1,2,3.
 xi is the ith coordinate, pi is the ith momentum.
 The other position and momentum quantities are denoted xj1, xj2, pj1 and pj2. These quantities appear in a symmetric way in the equation so in doesn't matter the order.*/
-mydbl geodesic_equation_i(mydbl p0, mydbl pi, mydbl pj1, mydbl pj2, mydbl t, mydbl xi, mydbl xj1, mydbl xj2)
+mydbl geodesic_equation_i(mydbl p0, mydbl pi, mydbl pj1, mydbl pj2, mydbl x0, mydbl xi, mydbl xj1, mydbl xj2)
 {
-  mydbl f =  -ith_der_potential(xi,xj1,xj2)*powl(p0,2)  + (1/powl(C,2))*(ith_der_potential(xi,xj1,xj2)*powl(pi,2) - ith_der_potential(xj1,xi,xj2)*powl(pj1,2) - ith_der_potential(xj2,xj1,xi)*powl(pj2,2)) + (2/powl(C,2))*(ith_der_potential(xj1,xi,xj2)*pi*pj1 + ith_der_potential(xj2,xi,xj1)*pi*pj2);
+  mydbl f =  (1/powl(C,2)) * ( 2*pi * (pi*ith_der_potential(xi,xj1,xj2) + pj1*ith_der_potential(xj1,xi,xj2) + pj2*ith_der_potential(xj2,xj1,xi)) - ith_der_potential(xi,xj1,xj2) * (powl(p0,2) + powl(pi,2) + powl(pj1,2) + powl(pj2,2)) ) ;
   return f;
 }
 
-void euler1(mydbl *t, mydbl *x1, mydbl *x2, mydbl *x3, mydbl *p0, mydbl *p1, mydbl *p2, mydbl *p3, mydbl *lambda)
+void euler1(mydbl *x0, mydbl *x1, mydbl *x2, mydbl *x3, mydbl *p0, mydbl *p1, mydbl *p2, mydbl *p3, mydbl *lambda)
 {
   /*Increment in the variables of the differential equation we want to solve*/
-  mydbl dt, dx1, dx2, dx3, dp0, dp1, dp2, dp3;
+  mydbl dx0, dx1, dx2, dx3, dp0, dp1, dp2, dp3;
 
   /*Calculation of the increments*/
-  dt = *p0*DLAMBDA; dx1 = *p1*DLAMBDA; dx2 = *p2*DLAMBDA; dx3 = *p3*DLAMBDA;
-  //  printf("%.10Lf %.10Lf %.10Lf %.10Lf\n",dt,dx1,dx2,dx3);
-  dp0 = geodesic_equation_0(*p0, *p1, *p2, *p3, *t, *x1, *x2, *x3)*DLAMBDA;
-  dp1 = geodesic_equation_i(*p0, *p1, *p2, *p3, *t, *x1, *x2, *x3)*DLAMBDA;
-  dp2 = geodesic_equation_i(*p0, *p2, *p1, *p3, *t, *x2, *x1, *x3)*DLAMBDA;
-  dp3 = geodesic_equation_i(*p0, *p3, *p2, *p1, *t, *x3, *x2, *x1)*DLAMBDA;
+  dx0 = *p0*DLAMBDA; dx1 = *p1*DLAMBDA; dx2 = *p2*DLAMBDA; dx3 = *p3*DLAMBDA;
+  dp0 = geodesic_equation_0(*p0, *p1, *p2, *p3, *x0, *x1, *x2, *x3)*DLAMBDA;
+  dp1 = geodesic_equation_i(*p0, *p1, *p2, *p3, *x0, *x1, *x2, *x3)*DLAMBDA;
+  dp2 = geodesic_equation_i(*p0, *p2, *p1, *p3, *x0, *x2, *x1, *x3)*DLAMBDA;
+  dp3 = geodesic_equation_i(*p0, *p3, *p2, *p1, *x0, *x3, *x2, *x1)*DLAMBDA;
 
   /*New values of the variables of the differential equation. Since we are using pointers, when called the routine the value of variable change.*/
-  *t = *t + dt; *x1 = *x1 + dx1; *x2 = *x2 + dx2; *x3 = *x3 + dx3;
+  *x0 = *x0 + dx0; *x1 = *x1 + dx1; *x2 = *x2 + dx2; *x3 = *x3 + dx3;
   *p0 = *p0 + dp0; *p1 = *p1 + dp1; *p2 = *p2 + dp2; *p3 = *p3 + dp3;
   
   /*Increment of parameter of geodesics*/
@@ -123,8 +122,8 @@ int main(void)
 
   
   /*Initial conditions*/
-  mydbl t = 0.0, x1 = 0.0, x2 = 0.0, x3 = 0.0, p0 = 1.0e-1, p1, p2 = 0.0, p3 = 0.0, lambda = 0.0, nu = 0.0;
-  p1 = -sqrtl(g00(x1,x2,x3)/gii(x1,x2,x3))*p0;
+  mydbl t = 0.0, x1 = 0.0, x2 = 0.0, x3 = 0.0, p0 = 0.001, p1 = p0, p2 = 0.0, p3 = 0.0, lambda = 0.0, nu = 0.0;
+  //p1 = -sqrtl(g00(x1,x2,x3)/gii(x1,x2,x3))*p0;
 
   /*Pointer to file where solution of differential equation will be saved.*/
   FILE *geodesic;
